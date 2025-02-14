5edce58a7f3290b5a5860ed7784a726ea338474a
mwilmes <maximilian.wilmes@hdi.de>
2025-02-14 11:49:05 +0100

    finish lombok challange

--- src/main/java/com/github/MaxWilmes1/Ecosystem/Lombok/CourseName.java
+++ src/main/java/com/github/MaxWilmes1/Ecosystem/Lombok/CourseName.java
@@ -1,10 +1,8 @@
 package com.github.MaxWilmes1.Ecosystem.Lombok;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum CourseName {
     MATH("Mathe"),
     IT("Informatik"),
@@ -14,7 +12,7 @@ public enum CourseName {
 
     private final String german;
 
-
-
-
+    CourseName(String german) {
+        this.german = german;
+    }
 }
