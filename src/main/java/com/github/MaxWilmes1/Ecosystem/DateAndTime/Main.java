5201f12c6b4db543819f3966faeca86cc31c3909
mwilmes <maximilian.wilmes@hdi.de>
2025-02-17 10:52:00 +0100

    add DayAndTime Challange

---
+++ src/main/java/com/github/MaxWilmes1/Ecosystem/DateAndTime/Main.java
@@ -0,0 +1,37 @@
+package com.github.MaxWilmes1.Ecosystem.DateAndTime;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Stepp 1 and 2
+        Instant instant = Instant.now();
+        System.out.println(instant);
+        Instant instantPlus2Weeks = instant.plus(Duration.ofDays(14));
+        System.out.println(instantPlus2Weeks);
+
+        System.out.println("--------------------");
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+        LocalDateTime localDateTimePlus2Weeks = localDateTime.plusWeeks(2);
+        LocalDateTime localDateTimePlus2Weeks2 = localDateTime.plus(2, ChronoUnit.WEEKS);
+        System.out.println(localDateTimePlus2Weeks2);
+
+        // Step 3
+        if (localDateTime.isBefore(localDateTimePlus2Weeks2)) {
+            System.out.println("Past");
+        } else {
+            System.out.println("Future");
+        }
+
+        // Step 4
+        long difference = ChronoUnit.DAYS.between(localDateTime, localDateTimePlus2Weeks2);
+        System.out.println(difference);
+
+    }
+}
